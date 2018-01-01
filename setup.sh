80dd086daa9e9044c7af93ae4002dab759f1006f
lion-man44 <lion44man@gmail.com>
2018-01-02 04:16:21 +0900

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
-ln -s ./dotfiles/Brewfile ~/Brewfile
-ln -s ./dotfiles/.vimrc ~/.vimrc
-ln -s ./dotfiles/.vim ~/.vim
-ln -s ./dotfiles/.zsh ~/.zsh
-ln -s ./dotfiles/.zprofile ~/.zprofile
-ln -s ./dotfiles/.zalias ~/.zalias
-ln -s ./dotfiles/.zshrc ~/.zshrc
-ln -s ./dotfiles/.gitconfig ~/.gitconfig
-ln -s ./dotfiles/.gemrc ~/.gemrc
-mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+ln -s ./dotfiles/Brewfile ~/Brewfile ;
+ln -s ./dotfiles/.vimrc ~/.vimrc ;
+ln -s ./dotfiles/.vim ~/.vim ;
+ln -s ./dotfiles/.zsh ~/.zsh ;
+ln -s ./dotfiles/.zprofile ~/.zprofile ;
+ln -s ./dotfiles/.zalias ~/.zalias ;
+ln -s ./dotfiles/.zshrc ~/.zshrc ;
+ln -s ./dotfiles/.gitconfig ~/.gitconfig ;
+ln -s ./dotfiles/.gemrc ~/.gemrc ;
+mkdir -p ~/.vim/bundle ;
+cd /tmp ;
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh ;
+sh installer.sh ~/.vim/bundle ;
+cd - ;
