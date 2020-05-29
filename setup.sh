baf0e8fab9930093ef9d8ccaa307eb77cf4661c3
lion-man44 <lion44man@gmail.com>
2020-05-29 10:00:11 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ ln -s ./dotfiles/.vim ~/.vim ;
 ln -s ./dotfiles/.zsh ~/.zsh ;
 ln -s ./dotfiles/.zprofile ~/.zprofile ;
 ln -s ./dotfiles/.zalias ~/.zalias ;
+ln -s ./dotfiles/.zscript ~/.zscript ;
 ln -s ./dotfiles/.zshrc ~/.zshrc ;
 ln -s ./dotfiles/.gitconfig ~/.gitconfig ;
 ln -s ./dotfiles/.gemrc ~/.gemrc ;
