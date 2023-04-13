27e83f5767cf2ea5c80650ae527fa42c2f8842d9
lion-man44 <amanttou44@gmail.com>
2023-04-13 18:01:00 +0900

    初期設定のupdate

--- setup.zsh
+++ setup.zsh
@@ -1,4 +1,3 @@
-ln -s ./dotfiles/Brewfile ~/Brewfile ;
 ln -s ./dotfiles/.vimrc ~/.vimrc ;
 ln -s ./dotfiles/.vim ~/.vim ;
 ln -s ./dotfiles/.zprofile ~/.zprofile ;
@@ -8,10 +7,11 @@ ln -s ./dotfiles/.zfunc ~/.zfunc ;
 ln -s ./dotfiles/.zshrc ~/.zshrc ;
 ln -s ./dotfiles/.gitconfig ~/.gitconfig ;
 ln -s ./dotfiles/.gemrc ~/.gemrc ;
+ln -s ./dotfiles/.agignore ~/.agignore ;
 mkdir -p ~/.vim/bundle ;
 cd /tmp ;
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh ;
 sh installer.sh ~/.vim/bundle ;
 ssh-keygen -t ed25519 -N '' ;
 cd ~/dotfiles/ ;
-#git clone git@github.com:mbadolato/iTerm2-Color-Schemes.git ;
+git clone git@github.com:mbadolato/iTerm2-Color-Schemes.git ;
